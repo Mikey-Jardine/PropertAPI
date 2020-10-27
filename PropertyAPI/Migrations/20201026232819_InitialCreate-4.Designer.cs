00afead5041bb759d81af0450b1daa18f1ee2fbb
Mikey-Jardine <mikeyjardine@gmail.com>
2020-10-27 00:00:45 +0000

    Migrations

--- PropertyAPI/Migrations/20201026232819_InitialCreate-4.Designer.cs
+++ PropertyAPI/Migrations/20201026232819_InitialCreate-4.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PropertyAPI.Models;
 
 namespace PropertyAPI.Migrations
 {
     [DbContext(typeof(AppDBContext))]
-    partial class AppDBContextModelSnapshot : ModelSnapshot
+    [Migration("20201026232819_InitialCreate-4")]
+    partial class InitialCreate4
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
