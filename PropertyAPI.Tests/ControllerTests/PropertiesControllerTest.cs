a0825f588cd5c767e4bddb4191ed465cf1f70b92
Mikey-Jardine <mikeyjardine@gmail.com>
2020-11-02 17:19:46 +0000

    Refactor

--- PropertyAPI.Tests/ControllerTests/PropertiesControllerTest.cs
+++ PropertyAPI.Tests/ControllerTests/PropertiesControllerTest.cs
@@ -17,98 +17,13 @@ namespace PropertyAPI.Tests.ControllerTests
 {
     public class PropertiesControllerTest : BaseControllerTests
     {
-
         private PropertiesController _controller;
-        private Mock<AppDBContext> _context;
         private AppDBContext context;
         private ServiceCollection services;
         private DbContextOptions<AppDBContext> _options;
-        public Mock<IConfiguration> Configuration { get; }
-
-        private static List<Photo> _photoList1 = new List<Photo>()
-        {
-            new Photo() { Id = 1, PropertyId = 1, Url = "https://photosa.propertyimages.ie/media/2/3/2/4292232/38e98b8e-645f-4adf-8e57-f927e5769840_l.jpg"},
-            new Photo() { Id = 2, PropertyId = 1, Url = "https://photosa.propertyimages.ie/media/2/3/2/4292232/38e98b8e-645f-4adf-8e57-f927e5769840_l.jpg"},
-            new Photo() { Id = 3, PropertyId = 1, Url = "https://photosa.propertyimages.ie/media/2/3/2/4292232/38e98b8e-645f-4adf-8e57-f927e5769840_l.jpg"}
-        };
-
-        private static List<Photo> _photoList2 = new List<Photo>()
-        {
-            new Photo() { Id = 4, PropertyId = 2, Url = "https://photosa.propertyimages.ie/media/2/3/2/4292232/38e98b8e-645f-4adf-8e57-f927e5769840_l.jpg"},
-            new Photo() { Id = 5, PropertyId = 2, Url = "https://photosa.propertyimages.ie/media/2/3/2/4292232/38e98b8e-645f-4adf-8e57-f927e5769840_l.jpg"},
-            new Photo() { Id = 6, PropertyId = 2, Url = "https://photosa.propertyimages.ie/media/2/3/2/4292232/38e98b8e-645f-4adf-8e57-f927e5769840_l.jpg"}
-        };
-
-        private static List<Photo> _photoList3 = new List<Photo>()
-        {
-            new Photo() { Id = 7, PropertyId = 3, Url = "https://photosa.propertyimages.ie/media/2/3/2/4292232/38e98b8e-645f-4adf-8e57-f927e5769840_l.jpg"},
-            new Photo() { Id = 8, PropertyId = 3, Url = "https://photosa.propertyimages.ie/media/2/3/2/4292232/38e98b8e-645f-4adf-8e57-f927e5769840_l.jpg"},
-            new Photo() { Id = 9, PropertyId = 3, Url = "https://photosa.propertyimages.ie/media/2/3/2/4292232/38e98b8e-645f-4adf-8e57-f927e5769840_l.jpg"}
-        };
-
-
-        private static Property _property1 = new Property()
-        {
-            Id = 1,
-            GroupLogoUrl = "https://photosa.propertyimages.ie/groups/9/0/4/6409/logo.jpg",
-            BedsString = "2 beds",
-            Price = 395000,
-            SizeStringMeters = 52.95,
-            DisplayAddress = "Apt.	16	The	Northumberlands,	Off	Lower	Mount	Street Dublin  2",
-            PropertyType = "Apartment",
-            BerRating = "D2",
-            MainPhoto =
-                "https://photosa.propertyimages.ie/media/2/3/2/4292232/38e98b8e-645f-4adf-8e57-f927e5769840_l.jpg",
-            Photos = new List<string>()
-            {
-                "https://photosa.propertyimages.ie/media/2/3/2/4292232/38e98b8e-645f-4adf-8e57-f927e5769840_l.jpg",
-                "https://photosa.propertyimages.ie/media/2/3/2/4292232/38e98b8e-645f-4adf-8e57-f927e5769840_l.jpg",
-                "https://photosa.propertyimages.ie/media/2/3/2/4292232/38e98b8e-645f-4adf-8e57-f927e5769840_l.jpg"
-            }
-        };
-
-        private static Property _property2 = new Property()
-        {
-            Id = 1,
-            GroupLogoUrl = "https://photosa.propertyimages.ie/groups/9/0/4/6409/logo.jpg",
-            BedsString = "2 beds",
-            Price = 395000,
-            SizeStringMeters = 52.95,
-            DisplayAddress = "Apt.	16	The	Northumberlands,	Off	Lower	Mount	Street Dublin  2",
-            PropertyType = "Apartment",
-            BerRating = "D2",
-            MainPhoto =
-                "https://photosa.propertyimages.ie/media/2/3/2/4292232/38e98b8e-645f-4adf-8e57-f927e5769840_l.jpg",
-            Photos = new List<string>()
-            {
-                "https://photosa.propertyimages.ie/media/2/3/2/4292232/38e98b8e-645f-4adf-8e57-f927e5769840_l.jpg",
-                "https://photosa.propertyimages.ie/media/2/3/2/4292232/38e98b8e-645f-4adf-8e57-f927e5769840_l.jpg",
-                "https://photosa.propertyimages.ie/media/2/3/2/4292232/38e98b8e-645f-4adf-8e57-f927e5769840_l.jpg"
-            }
-        };
-
-        private static Property _property3 = new Property()
-        {
-            Id = 1,
-            GroupLogoUrl = "https://photosa.propertyimages.ie/groups/9/0/4/6409/logo.jpg",
-            BedsString = "2 beds",
-            Price = 395000,
-            SizeStringMeters = 52.95,
-            DisplayAddress = "Apt.	16	The	Northumberlands,	Off	Lower	Mount	Street Dublin  2",
-            PropertyType = "Apartment",
-            BerRating = "D2",
-            MainPhoto =
-                "https://photosa.propertyimages.ie/media/2/3/2/4292232/38e98b8e-645f-4adf-8e57-f927e5769840_l.jpg",
-            Photos = new List<string>()
-            {
-                "https://photosa.propertyimages.ie/media/2/3/2/4292232/38e98b8e-645f-4adf-8e57-f927e5769840_l.jpg",
-                "https://photosa.propertyimages.ie/media/2/3/2/4292232/38e98b8e-645f-4adf-8e57-f927e5769840_l.jpg",
-                "https://photosa.propertyimages.ie/media/2/3/2/4292232/38e98b8e-645f-4adf-8e57-f927e5769840_l.jpg"
-            }
-        };
-
-        private List<Property> properties = new List<Property>() { _property1, _property2, _property3 };
         private ServiceProvider serviceProvider;
+        public IPropertyService propertyService { get; set; }
+        public IPropertyRepository propertyRepository { get; set; }
 
         public void InitializeDbContextServices()
         {
@@ -133,8 +48,8 @@ namespace PropertyAPI.Tests.ControllerTests
 
             services.AddDbContext<AppDBContext>(builder =>
             {
-                var connStr = _configuration.GetConnectionString("AppDBContext");
-                builder.UseSqlServer(connStr, opt =>
+                var connectionString = _configuration.GetConnectionString("AppDBContext");
+                builder.UseSqlServer(connectionString, opt =>
                 {
                     opt.EnableRetryOnFailure();
                     opt.CommandTimeout(15);
@@ -151,8 +66,8 @@ namespace PropertyAPI.Tests.ControllerTests
 
             serviceProvider = services.BuildServiceProvider();
             context = serviceProvider.GetRequiredService<AppDBContext>();
-            var propertyService = serviceProvider.GetRequiredService<IPropertyService>();
-            var propertyRepository = serviceProvider.GetRequiredService<IPropertyRepository>();
+            propertyService = serviceProvider.GetRequiredService<IPropertyService>();
+            propertyRepository = serviceProvider.GetRequiredService<IPropertyRepository>();
 
             _controller = new PropertiesController(context, propertyService, propertyRepository);
         }
